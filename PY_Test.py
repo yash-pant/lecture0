a368b5c276563562d46e5cb72f26872f10a1537e
Yash Pant <yash.pant727@outlook.com>
2020-04-26 21:14:00 -0700

    Added new python program

---
+++ PY_Test.py
@@ -0,0 +1,6 @@
+import random
+count = 0
+while count < 20:
+    r = random.randint(1,400)
+    print("Yash is a legend. And Aayush too. But Yash is " + str(r) + " times better than Aayush.")
+    count += 2
